408bc7780557557fc3d4968df6f8f45b6d406a58
Dealer327 <88377783+Dealer327@users.noreply.github.com>
2022-09-15 16:39:57 +0300

    Первый блин

---
+++ 09.11/1.py
@@ -0,0 +1,165 @@
+import enum
+
+
+class Gender(enum):
+    MALE = 'man'
+    FEMALE = 'woman'
+
+
+class Degree(enum):
+    BACHELOR = 'Bac'
+    MASTER = 'Mas'
+    DOCTOR = 'Doc'
+
+
+class Person:
+    def __init__(self, name: str,
+                 birthdate: str,
+                 gender: Gender):
+        self.gender = gender
+        self.birthdate = birthdate
+        self.__name = name
+
+    @property
+    def name(self):
+        return self.__name
+
+
+class Employee(Person):
+    def __init__(self,
+                 name: str,
+                 birthdate: str,
+                 gender: Gender,
+                 position: str,
+                 salary: int):
+        super().__init__(name, birthdate, gender)
+        self.position = position
+        self.salary = salary
+
+
+class Administator(Employee):
+    def __init__(self,
+                 name: str,
+                 birthdate: str,
+                 gender: Gender,
+                 position: str,
+                 salary: int,
+                 # supervisor: Administator,
+                 subordinates=None):
+        super().__init__(name, birthdate, gender, position, salary)
+        if subordinates is None:
+            subordinates = []
+        self.subordinates = subordinates
+
+
+class ProfessionalEmployee(Employee):
+    def __init__(self,
+                 name: str,
+                 birthdate: str,
+                 gender: Gender,
+                 position: str,
+                 salary: int,
+                 degree: Degree.BACHELOR,
+                 experience: int = 0):
+        super().__init__(name, birthdate, gender, position, salary)
+        self.degree = degree
+        self.experience = experience
+
+
+class Teacher(ProfessionalEmployee):
+    def __init__(self,
+                 name: str,
+                 birthdate: str,
+                 gender: Gender,
+                 position: str,
+                 salary: int,
+                 degree: Degree = Degree.MASTER,
+                 professorship: bool = False,
+                 experience: int = 0,
+                 course=None):
+        super().__init__(name, birthdate, gender, position, salary, experience)
+        if course is None:
+            course = []
+        self.__course = course
+        self.professorship = professorship
+        self.degree = degree
+
+    def add_course(self, course: str):
+        self.__course.append(course)
+        # self.__rem_course.remove(course)
+
+
+class Researcher(ProfessionalEmployee):
+    def __init__(self,
+                 name: str,
+                 birthdate: str,
+                 gender: Gender,
+                 position: str,
+                 salary: int,
+                 experience: int = 0,
+                 degree: Degree = Degree.MASTER):
+        super().__init__(name, birthdate, gender, position, salary, experience)
+        self.degree = degree
+
+
+class GeneralPersonnel(Employee):
+    pass
+
+
+class SecurityPersonnel(Employee):
+    pass
+
+
+class EducationForms(enum):
+    INTRAMURAL = 'Intra'
+    EXTRAMURAL = 'Extra'
+    REMOTE = 'Rem'
+
+
+class Student(Person):
+    def __init__(self,
+                 name: str,
+                 birthdate: str,
+                 gender: Gender,
+                 form: EducationForms.INTRAMURAL,
+                 year: int = 1,
+                 average_grade: float = -1):
+        super().__init__(name, birthdate, gender)
+        self.average_grade = average_grade
+        self.year = year
+        self.form = form
+
+    def __str__(self):
+        pass
+
+    def year_increment(self):
+        pass
+
+
+class OrganizationLevel:
+    def __init__(self,
+                 name: str,
+                 head: Administator,
+                 employees: list[Employee],
+                 address: str
+                 # budget
+                 ):
+        self.address = address
+        self.__employees = employees
+        self.__head = head
+        self.name = name
+
+    def __str__(self):
+        pass
+
+    def change_head(self, Administrator):
+        pass
+
+    def hire_employee(self, Employee):
+        pass
+
+    def fire_employee(self, Employee):
+        pass
+
+    def set_annual_budget(self, amount: int):
+        pass
