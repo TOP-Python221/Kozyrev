a6938b55331c38d993e99fcc74e4bc38ef9c1f70
Gennadiy <renardes@gmail.com>
2022-09-27 14:36:26 +0500

    08.21: корректировка использования импорта

--- 08.21/2.py
+++ 08.21/2.py
@@ -1,24 +1,19 @@
-import datetime
+from datetime import datetime as dt, timedelta, timezone
 
 
 class Utc:
-    TimeOfDay = {'00': 'Ночь', '01': 'Ночь', '02': 'Ночь',
-                 '03': 'Ночь', '04': 'Ночь', '05': 'Ночь', '06': 'Утро',
-                 '07': 'Утро', '08': 'Утро', '09': 'Утро', '10': 'Утро',
-                 '11': 'Утро', '12': 'День', '13': 'День', '14': 'День', '15': 'День',
-                 '16': 'День', '17': 'День', '18': 'Вечер', '19': 'Вечер', '20': 'Вечер',
-                 '21': 'Вечер', '22': 'Вечер', '23': 'Вечер'}
+    TimeOfDay = {'00': 'Ночь', '01': 'Ночь', '02': 'Ночь', '03': 'Ночь', '04': 'Ночь', '05': 'Ночь', '06': 'Утро', '07': 'Утро', '08': 'Утро', '09': 'Утро', '10': 'Утро', '11': 'Утро', '12': 'День', '13': 'День', '14': 'День', '15': 'День', '16': 'День', '17': 'День', '18': 'Вечер', '19': 'Вечер', '20': 'Вечер', '21': 'Вечер', '22': 'Вечер', '23': 'Вечер'}
 
     def __init__(self, hours: int):
         self.hours = hours
-        self.td = datetime.timedelta(hours=self.hours)
-        self.tz = datetime.timezone(self.td)
-        self.tn = datetime.datetime.now(self.tz)
-        self.hd = datetime.datetime.strftime(self.tn, "%H")
+        self.td = timedelta(hours=self.hours)
+        self.tz = timezone(self.td)
+        self.tn = dt.now(self.tz)
+        self.hd = dt.strftime(self.tn, "%H")
 
     def __str__(self):
         return f'{Utc.TimeOfDay[self.hd]}\n' \
-               f'{datetime.datetime.strftime(self.tn, "%H:%M")}'
+               f'{dt.strftime(self.tn, "%H:%M")}'
 
 
 u1 = Utc(3)
