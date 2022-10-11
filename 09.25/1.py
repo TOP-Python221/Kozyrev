2b990fde3728ad186a685b9b7bd78eda78e921d9
Dealer327 <88377783+Dealer327@users.noreply.github.com>
2022-10-11 18:10:48 +0300

    Present pattern builder

---
+++ 09.25/1.py
@@ -0,0 +1,45 @@
+class Add_class:
+    indent_size = 4
+    indent = 0
+
+    def __init__(self):
+        self.name_class = ''
+        self.designer_class = []
+
+    def __str__(self):
+        return self.__str
+
+    @property
+    def __str(self):
+        designer = 'def __init__(self):'
+        lines = ' ' * (self.indent + 1) * self.indent_size
+        new_designer = ''
+        re = ''
+        new_class = f'class {self.name_class}:'
+        if self.designer_class:
+            for el in self.designer_class:
+                re += f'{lines + lines}{el}\n'
+            new_designer += f'{new_class}\n{lines}{designer}\n{re}'
+        else:
+            new_designer = f'{new_class}\n{lines}pass'
+        return new_designer
+
+
+class Builder_class(Add_class):
+    def __init__(self, name):
+        super().__init__()
+        self.name_class = name
+        self.add = Add_class()
+
+    def add_field(self, parameter: str, meaning: str = "''"):
+        self.designer_class.append(f'self.{parameter} = {meaning}')
+        return self
+
+
+Ruslan = Builder_class('Person').add_field('name', 'Ruslan').add_field('age', )
+
+print(Ruslan)
+
+random_class = Builder_class('Foo')
+
+print(random_class)
